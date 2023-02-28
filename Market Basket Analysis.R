1f0094c26da517b5305c425b861e8c2abf3d9ed7
Yohanes Candra Kusumaputra <117979570+yohanescandra08@users.noreply.github.com>
2023-02-28 10:27:47 +0700

    Add files via upload

---
+++ Market Basket Analysis.R
@@ -0,0 +1,12 @@
+dqlab.trans <-
+  read.transactions(
+    file = "https://storage.googleapis.com/dqlab-dataset/transaksi_dqlab_retail.tsv",
+    format = "single",
+    sep = "\t",
+    cols = c(1, 2),
+    skip = 1
+  )
+print(dqlab.trans)
+dqlab.trans <-
+  itemFrequency(dqlab.trans, type = "absolute")
+print(dqlab.trans)
