60592f1c4f1c7ca07a0073561bf821e4f0ba6d85
yohanescandra08 <yohanescandra07@gmail.com>
2023-03-06 15:53:21 +0700

    R code update

--- Market Basket Analysis.R
+++ Market Basket Analysis.R
@@ -1,7 +1,6 @@
 # 1. Packages List --------------------------------------------------------
 library(tidyverse)
 library(arules)
-library(arulesViz)
 library(plotly)
 library(RColorBrewer)
 library(reactablefmtr)
@@ -60,11 +59,12 @@ dqlab.exp.plot <-
   dqlab.exp %>%
   mutate(Rank = 1:nrow(dqlab.exp), .before = 1) %>%
   mutate(
+    Percentage = dqlab.exp$Total / 3450,
     Top_bottom =
       c(
-        paste("Top", 1:10),
+        rep(paste("Top", 10), times = 10),
         rep(NA, each = nrow(dqlab.exp) - 20),
-        paste("Bottom", 1:10)
+        rep(paste("Bottom", 10), times = 10)
       ),
     Has_missing = "",
     Colors = rep(c("green", "grey", "red"), times = c(10, (nrow(
@@ -88,28 +88,38 @@ dqlab.exp.plot <-
         Product_Name = colDef(name = "Product Name",
                               align = "left"),
         Total = colDef(
-          name = "Total Unit",
+          name = "Transaction Frequency",
           align = "center",
-          cell = data_bars(
+          cell = color_tiles(
             data = dqlab.exp.plot,
-            fill_color = brewer.pal(5, "BrBG"),
-            round_edges = T,
-            text_position = "outside-end",
-            background = "transperent",
-            box_shadow = T,
-            bar_height = 5
+            colors = brewer.pal(5, "RdYlGn"),
+            box_shadow = T
           )
         ),
         Top_bottom = colDef(
           name = "Top 10 / Bottom 10",
           align = "center",
-          cell = pill_buttons(
+          cell = color_tiles(
             data = dqlab.exp.plot,
             color_ref = "Colors",
             box_shadow = T
           )
         ),
         Colors = colDef(show = F),
+        Percentage = colDef(
+          name = "Transaction Percentage",
+          align = "center",
+          cell = data_bars(
+            data = dqlab.exp.plot,
+            fill_color = brewer.pal(5, "RdYlGn"),
+            round_edges = T,
+            text_position = "outside-end",
+            background = "transperent",
+            box_shadow = T,
+            bar_height = 5,
+            number_fmt = scales::label_percent(decimal.mark = ",", accuracy = 0.01)
+          )
+        ),
         Has_missing = colDef(
           show = FALSE,
           filterMethod = JS(
@@ -131,151 +141,98 @@ dqlab.exp.plot <-
   ))
 dqlab.exp.plot
 
-# * * 3.1.1. Visualization ------------------------------------------------
-color.1 <- brewer.pal(n = 9, name = "Greens")
-dqlab.trans.freq.top.10.plot <-
-  plot_ly(dqlab.trans.freq.top.10) %>%
-  add_trace(
-    x = ~ Total,
-    y = ~ Product_Name,
-    type = "bar",
-    color = ~ Product_Name,
-    colors = color.1,
-    hovertemplate = "<i>%{y}</i><br><b>%{x} Units</b></br><extra></extra>",
-    textposition = "none"
-  ) %>%
-  layout(
-    showlegend = F,
-    xaxis = list(title = "Total Unit",
-                 range = c(0, 2075)),
-    yaxis = list(title = "Fashion Item"),
-    title = "Top 10 Fashion Item Sell"
-  )
-dqlab.trans.freq.top.10.plot
-# * * 3.2.1. Visualization ------------------------------------------------
-color.2 <- rev(brewer.pal(n = 9, name = "Reds"))
-dqlab.trans.freq.bottom.10.plot <-
-  plot_ly(dqlab.trans.freq.bottom.10) %>%
-  add_trace(
-    x = ~ Total,
-    y = ~ Product_Name,
-    type = "bar",
-    color = ~ Product_Name,
-    colors = color.2,
-    hovertemplate = "<i>%{y}</i><br><b>%{x} Units</b></br><extra></extra>",
-    textposition = "none"
-  ) %>%
-  layout(
-    showlegend = F,
-    xaxis = list(title = "Total Unit",
-                 range = c(0, 2075)),
-    yaxis = list(title = "Fashion Item"),
-    title = "Bottom 10 Fashion Item Sell"
-  )
-dqlab.trans.freq.bottom.10.plot
 
-
-# 4. Products Bundles Combination for Bottom 10 Fashion Item --------------
-dqlab.trans.combi.bottom.10 <-
-  apriori(dqlab.trans.1,
+# 4. Product Bundle Combinations for Bottom 10 Fashion Item ---------------
+dqlab.exp.bottom.10.combi <-
+  apriori(dqlab.trans,
           parameter = list(
-            supp = 9 / length(dqlab.trans.1),
+            supp = 9 / length(dqlab.trans),
             confidence = 0.1,
             minlen = 2,
             maxlen = 3
           ))
-dqlab.trans.combi.bottom.10 <-
+dqlab.exp.bottom.10.combi <-
   c(
     head(sort(
-      subset(
-        dqlab.trans.combi.bottom.10,
-        (rhs %in% "Celana Jeans Sobek Pria")
-      ),
+      subset(dqlab.exp.bottom.10.combi,
+             (rhs %in% "Celana Jeans Sobek Pria")),
       by = "lift"
     ), n = 1),
     head(sort(
-      subset(dqlab.trans.combi.bottom.10,
+      subset(dqlab.exp.bottom.10.combi,
              (rhs %in% "Tas Kosmetik")),
       by = "lift"
     ),
     n = 1),
     head(sort(
-      subset(dqlab.trans.combi.bottom.10,
+      subset(dqlab.exp.bottom.10.combi,
              (rhs %in% "Stripe Pants")),
       by = "lift"
     ),
     n = 1),
     head(sort(
-      subset(dqlab.trans.combi.bottom.10,
+      subset(dqlab.exp.bottom.10.combi,
              (rhs %in% "Pelembab")),
       by = "lift"
     ),
     n = 1),
     head(sort(
-      subset(dqlab.trans.combi.bottom.10,
+      subset(dqlab.exp.bottom.10.combi,
              (rhs %in% "Tali Ban Ikat Pinggang")),
       by = "lift"
     ),
     n = 1),
     head(sort(
       subset(
-        dqlab.trans.combi.bottom.10,
+        dqlab.exp.bottom.10.combi,
         (rhs %in% "Baju Renang Pria Anak-anak")
       ),
       by = "lift"
     ),
     n = 1),
     head(sort(
-      subset(dqlab.trans.combi.bottom.10,
+      subset(dqlab.exp.bottom.10.combi,
              (rhs %in% "Hair Dye")),
       by = "lift"
     ),
     n = 1),
     head(sort(
-      subset(dqlab.trans.combi.bottom.10,
+      subset(dqlab.exp.bottom.10.combi,
              (rhs %in% "Atasan Baju Belang")),
       by = "lift"
     ),
     n = 1),
     head(sort(
       subset(
-        dqlab.trans.combi.bottom.10,
+        dqlab.exp.bottom.10.combi,
         (rhs %in% "Tas Sekolah Anak Perempuan")
       ),
       by = "lift"
     ),
     n = 1),
     head(sort(
-      subset(dqlab.trans.combi.bottom.10,
+      subset(dqlab.exp.bottom.10.combi,
              (rhs %in% "Dompet Unisex")),
       by = "lift"
     ),
     n = 1)
   )
-dqlab.trans.combi.bottom.10 <-
-  DATAFRAME(dqlab.trans.combi.bottom.10)
-View(dqlab.trans.combi.bottom.10)
-write.csv(dqlab.trans.combi.bottom.10,
-          file = "Products Bundles for Bottom 10 Fashion Item.txt")
+dqlab.exp.bottom.10.combi <-
+  DATAFRAME(dqlab.exp.bottom.10.combi)
+View(dqlab.exp.bottom.10.combi)
+write.csv(dqlab.exp.bottom.10.combi,
+          file = "Product Bundle Combinations for Bottom 10 Fashion Item.txt")
 # * 4.1. Visualization ----------------------------------------------------
-dqlab.trans.combi.bottom.10 <-
-  dqlab.trans.combi.bottom.10 %>%
+dqlab.exp.bottom.10.combi <-
+  dqlab.exp.bottom.10.combi %>%
   arrange(desc(lift))
-nrow(dqlab.trans.combi.bottom.10)
-dqlab.trans.combi.bottom.10$rule <-
+nrow(dqlab.exp.bottom.10.combi)
+dqlab.exp.bottom.10.combi$rule <-
   paste("Rule", 1:6)
-dqlab.trans.combi.bottom.10$support <-
-  round(dqlab.trans.combi.bottom.10$support, digits = 3)
-dqlab.trans.combi.bottom.10$confidence <-
-  round(dqlab.trans.combi.bottom.10$confidence, digits = 3)
-dqlab.trans.combi.bottom.10$coverage <-
-  round(dqlab.trans.combi.bottom.10$coverage, digits = 3)
-dqlab.trans.combi.bottom.10$lift <-
-  round(dqlab.trans.combi.bottom.10$lift, digits = 3)
-dqlab.trans.combi.bottom.10.plot <-
+dqlab.exp.bottom.10.combi.plot <-
   ggplotly(
     ggplot(
-      dqlab.trans.combi.bottom.10,
+      dqlab.exp.bottom.10.combi,
       aes(
         x = str_wrap(LHS, 25),
         y = str_wrap(RHS, 15),
@@ -289,17 +246,20 @@ dqlab.trans.combi.bottom.10.plot <-
           RHS,
           "<br>",
           "Lift : ",
-          lift,
+          round(lift, 2),
           "</b>",
           "<br>",
           "Support : ",
-          support,
+          round(support, 4) * 100,
+          "%",
           "<br>",
           "Confidence : ",
-          confidence,
+          round(confidence, 4) * 100,
+          "%",
           "<br>",
           "Coverage : ",
-          coverage,
+          round(coverage, 4) * 100,
+          "%",
           "</br>"
         )
       )
@@ -316,7 +276,7 @@ dqlab.trans.combi.bottom.10.plot <-
         direction = 1
       ) +
       labs(
-        title = "Product Bundles for Bottom 10 Fashion Item",
+        title = "Product Bundle Combinations for Bottom 10 Fashion Item",
         x = "LHS",
         y = "RHS",
         fill = "Lift"
@@ -325,7 +285,7 @@ dqlab.trans.combi.bottom.10.plot <-
     ,
     tooltip = c("text")
   )
-dqlab.trans.combi.bottom.10.plot
+dqlab.exp.bottom.10.combi.plot
 
 
 # 5. Products Bundles Based on Requested Filters --------------------------
